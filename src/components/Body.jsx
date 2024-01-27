2408bca318bb0742438788d4742702eb604b46c0
Aman <amankunalbharti2003@gmail.com>
2024-01-28 02:42:24 +0530

    api fetching but problem in data fetching giving error

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,31 +1,67 @@
-import { useState } from "react";
-import resLists from "../utils/mockdata";
+import { useEffect, useState } from "react";
 import FoodCard from "./Foodcard";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
+  const [listofres, setListOfRes] = useState([]);
+  const [filteredres, setfilteredres]= useState([]);
+  const [searchfood, setSearchFood] = useState("");
 
-  const [listofres,setListOfRes] = useState(resLists);
-  
-  return (
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.6932429&lng=88.37925919999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+
+    const json = await data.json();
+    console.log(json);
+    //optional Chaining
+    setListOfRes(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+    setfilteredres(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+  };
+
+
+
+  return listofres.length === 0 ?( <Shimmer />): (
     <div className="body">
       <div className="search">
-        {/* <input type="text" placeholder="Search for food and resturants"></input> */}
-        <button onClick={()=>{
-          const filterRes = listofres.filter((res)=>
-            res.info.avgRating > 4
-          );
+        <input
+          type="text"
+          placeholder="Search for food and resturants"
+          value={searchfood}
+          onChange={(e) => {
+            setSearchFood(e.target.value);
+          }}
+        ></input>
+        <button
+          onClick={() => {
+            console.log(listofres)
+            const filteredfood = listofres.filter((res) => 
+              
+            res?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants?.info?.name?.toLowerCase(). includes(searchfood.toLowerCase())
+            );
+            console.log(filteredfood)
+            setfilteredres(filteredfood);
+          }}
+        >Search</button>
+        <button
+          onClick={() => {
+            const filterRes = listofres.filter(
+              (res) => res.info.avgRating >= 4.5
+            );
 
-          setListOfRes(filterRes);
-
-        }}
-          
+            setListOfRes(filterRes);
+          }}
         >
           Filter With Highest Rating
         </button>
       </div>
 
       <div className="res-container">
-        {listofres.map((info, index) => (
+        {filteredres.map((info, index) => (
           <FoodCard key={index} resData={info} />
         ))}
       </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,37 @@
+import { useState } from "react";
 import { LOGO_URL } from "../utils/constants";
 
 const Header = () => {
-    return (
-      <div className="header">
-        <div className="logo">
-          <img src={LOGO_URL} />
-        </div>
-  
-        <div className="nav-items">
-          <ul className="list-item">
-            <li>Home</li>
-            <li>About Us</li>
-            <li>Countact Us</li>
-            <li>CART</li>
-          </ul>
-        </div>
-      </div>
-    );
-  };
+  const [loginbtn, setLogInBtn] = useState("Log In");
 
-  export default Header;
+  return (
+    <div className="header">
+      <div className="logo">
+        <img src={LOGO_URL} />
+      </div>
+
+      <div className="nav-items">
+        <ul className="list-item">
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Countact Us</li>
+          <li>
+            <button
+              className="login"
+              onClick={() => {
+                loginbtn === "Log In"
+                  ? setLogInBtn("Log Out")
+                  : setLogInBtn("Log In");
+              }}
+            >
+              {loginbtn}
+            </button>
+          </li>
+          <li>CART</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
